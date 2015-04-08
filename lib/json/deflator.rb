90c3c9dec7bc58b5d8bbcd80522bcb5d6ff3edff
Luis Canales <kopz9999@gmail.com>
2015-04-08 09:27:26 -0400

    kickoff

---
+++ lib/json/deflator.rb
@@ -0,0 +1,7 @@
+require "json/deflator/version"
+
+module Json
+  module Deflator
+    # Your code goes here...
+  end
+end
---
+++ lib/json/deflator/version.rb
@@ -0,0 +1,5 @@
+module Json
+  module Deflator
+    VERSION = "0.1.0"
+  end
+end
