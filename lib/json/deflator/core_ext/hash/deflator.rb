8b1f6cbaf7490fb7442b7245b8a49f3bacf03476
Luis Canales <kopz9999@gmail.com>
2015-04-30 16:56:24 -0400

    Added standard filtering for except or only keys

--- lib/json/deflator/core_ext/hash/deflator.rb
+++ lib/json/deflator/core_ext/hash/deflator.rb
@@ -6,6 +6,7 @@ class Hash
 
   def deflate_jpath!( opts = {} )
     base_path = Json::Deflator::ObjectManager.current_instance.current_path
+    standard_filtering! opts
     self.each do | k, v |
       Json::Deflator::ObjectManager.current_instance.current_path = 
         "#{ base_path }.#{ k }"
@@ -14,8 +15,19 @@ class Hash
   end
 
   def deflate_static_reference!( opts = {} )
+    standard_filtering! opts
     self.each do | k, v |
       self[k] = v.deflate_json!(opts)
+    end
+  end
+
+  def standard_filtering!( options )
+    if options
+      if attrs = options[:only]
+        self.slice!(*Array(attrs))
+      elsif attrs = options[:except]
+        self.except!(*Array(attrs))
+      end
     end
   end
 
